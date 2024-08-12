efbeeecddf001d7c558326a72cd0227eccf63e29
Yassine Elmellouki <yelmellouki@gmail.com>
2024-08-12 11:41:32 -0400

    adjustments made pre demo

---
+++ assets/js/modal.js
@@ -0,0 +1,29 @@
+// Get the modal
+var modal = document.getElementById("insuranceModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Show the modal when the page loads
+window.onload = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Handle form submission (you can replace this with actual form handling code)
+document.getElementById("insuranceForm").onsubmit = function (event) {
+  event.preventDefault();
+  alert("Form submitted!");
+  modal.style.display = "none";
+};
